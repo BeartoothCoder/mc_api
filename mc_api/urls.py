85436fca42be124e8f84b76f19384e9ff4bf62de
Aiden Boll <aidenboll@yahoo.com>
2025-06-26 10:10:42 -0600

    Add home page with micro-docs

--- mc_api/urls.py
+++ mc_api/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.shortcuts import render
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('recipes/', include('recipes.urls', namespace='recipes')),
+    path('', lambda req : render(req, 'home.html', {'ip':req.get_host()})),
 ]
