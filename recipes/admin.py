64a05b0536e20089d4b3034bf270d5f94d0cf516
Aiden Boll <aidenboll@yahoo.com>
2025-06-26 10:11:40 -0600

    Add model admin. Make sure you create a superuser.

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Recipe
 
 # Register your models here.
+@admin.register(Recipe)
+class RecipeModel(admin.ModelAdmin):
+    fields = ['data', 'img_link']
--- recipes/models.py
+++ recipes/models.py
@@ -6,3 +6,9 @@ class Recipe(m.Model):
     name = m.CharField(max_length=100, null=True)
     data = m.JSONField()
     img_link = m.CharField(max_length=150, blank=True, null=True)
+
+    def __str__(self):
+        right = str(self.data).split("'description': {'identifier': 'minecraft:")[-1]
+        name = right.split("'", 1)[0]
+        name = name.replace('_', ' ').title()
+        return name
